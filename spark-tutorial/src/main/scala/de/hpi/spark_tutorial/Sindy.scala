1238d869f96e77f0c8384a7b1cbbb04e23c06260
lisakoeritz <lisa.koeritz@student.hpi.de>
2021-07-08 18:29:39 +0200

    implement SINDY - first try

--- spark-tutorial/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ spark-tutorial/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,41 +1,75 @@
 package de.hpi.spark_tutorial
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, collect_set, explode}
 
 object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+    import spark.implicits._
 
-    // TODO
-    //Input Tuples --> Cells
-    //  - Splitting each record with (value, (Singleton type))
+    //1 - input data
+    val datasets = inputs.map(filename => { //Reading csv files
+      spark.read
+        .option("inferSchema", "true")
+        .option("header", "true")
+        .option("delimiter", ";")
+        .csv(filename)
+    })
+    //print(datasets)
 
-    //Cells --> Cache-based Preaggregation (optional when repeatedly occuring cells with values)
-    //- Should reduce network load
-    //  - Groups by the value, and the singleton type
+    //2 - split each record into cells: tuples with a value and a singleton set -> (value, {column_name})
+    val columnList = datasets.flatMap(data => data.columns.map(data.select(_))) //get columns from dataset into one dataset format
+    //columnList.foreach(row => row.show())
 
-    //Cache-based Preaggregation --> Global partitioning
-//    - Reordering the cells among the workers of the cluster through hashing
-//    - We need an appropriate function to map each different value to unique cell
-//    - Therefore: cells with the same value are on the same worker
-
-//      Global partitioning --> Attribute Sets
-//    - Grouping all cells by their values
-//      - Aggregating attribute sets using union operator
-
-//    Attribute Sets -->  Inclusion Lists
-//    - Set with n attributes = n inclusion lists (all possible combinations)
-
-//    Inclusion List --> Partition
-//      - Group by the first attribute
-
-//    Partition --> Aggregate
-//    - Intersection with preaggregation
-//    - Ends with attributes with empty sets; no (n,0)
+    val tuples = columnList.map(column => column.map(row => (row.get(0).toString, row.schema.fieldNames.head)))
+    val cells = tuples.reduce((d1, d2) => d1.union(d2)) // reduce to apply concatenation of datasets in list
+    //cells.show()
 
 
-//    Aggregate --> INDs
-//    - Disassembling into INDs
+    //3 - cache-based preaggregation: group by value and union attribute sets
+    val preagg = cells.toDF("value", "attr_name")
+      .groupBy("value")
+      .agg(collect_set("attr_name").as("attributes"))
+    //preagg.show()
+
+
+    //4 - reorder all cells among the workers of the cluster
+    // Spark automatically partitions RDDs
+
+   
+    // 5 - discard value, create attribute sets
+    /*val attrSets = preagg.withColumn("attributes", explode(col("attributes")))
+      .groupBy("value")
+      .agg(collect_set("attributes").as("attr_set")) 
+      .select("attr_set")*/ //deconstruct and reconstruct to merge all worker results: necessary??
+    val attrSets = preagg.select("attributes").distinct().as[Seq[String]]
+    //attrSets.show()
+
+    // 6 - create inclusion lists
+    val inclusionLists = attrSets
+      .select(explode(col("attributes")).as("key"), col("attributes"))
+      .as[(String, Seq[String])]
+      .map(row => (row._1, row._2.filter(_ != row._1)))
+
+    //inclusionLists.show()
+
+    // 7 - group by key + intersect attribute sets
+    val aggInclList = inclusionLists.toDF("key", "ind_candidates")
+      .groupBy("key")
+      .agg(collect_set("ind_candidates").as("ind_candidates")).as[(String, Seq[Seq[String]])]
+    //aggInclList.show()
+
+    val intersectedInclList = aggInclList.map(row => (row._1, row._2.reduce(_.intersect(_)))) //does this make sense?!
+    val filteredINDList = intersectedInclList.filter(row => row._2.nonEmpty)
+    //filteredINDList.show()
+
+
+    // 8 - sort and output INDs
+    val output = filteredINDList.toDF("dependent", "referenced_IND").sort("dependent").as[(String,Seq[String])]
+    //output.show()
+
+    output.collect().foreach(row => println(row._1 + " > " + row._2.reduce(_ + " , " + _)))
 
 
   }
